62b3d92f1836247318da8275d1113c5b6be3e458
Tymon Szczepanowski <tymon.szczepanowski@gmail.com>
2021-07-08 21:23:57 +0200

    something wrong with classifying

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from sklearn.datasets import load_iris, load_wine, load_breast_cancer
-import selectors_lib
+import selectors_lib as slct
 
 if __name__ == '__main__':
-    print("hello world!")
     iris = load_iris()
     wine = load_wine()
     breast_cancer = load_breast_cancer()
@@ -12,14 +11,13 @@ if __name__ == '__main__':
     
     for i in range(len(data)):
         print("\n" + data[i] + ", " + str(len(X[i][0])) + " features:")
+        X[i], y[i] = slct.randomize_data(X[i], y[i]) 
+        forward_search = slct.forward_search(X[i], y[i])
+        variance_threshold = slct.variance_threshold(X[i])
+        no_selectors = slct.knn_score(X[i], y[i]) 
+        forward_selector = slct.knn_score(forward_search, y[i]) 
+        variance_selector = slct.knn_score(variance_threshold, y[i])
         
-        forward_search = selectors.forward_search(X[i], y[i])
-        variance_threshold = selectors.variance_threshold(X[i])
-
-        no_selectors = selectors.get_score(X[i], y[i]) 
-        forward_selector = selectors.get_score(forward_search, y[i]) 
-        variance_selector = selectors.get_score(variance_threshold, y[i])
-        
-        print("No selectors: " + str(selectors.get_score(X[i], y[i])) + "%")
+        print("No selectors: " + str(slct.knn_score(X[i], y[i])) + "%")
         print("Forward search selector: " + str(forward_selector) + "%")
         print("Variance threshold selector: " + str(variance_selector) + "%")
--- selectors_lib.py
+++ selectors_lib.py
@@ -1,11 +1,21 @@
 import numpy as np
+from random import randrange 
 from sklearn.neighbors import KNeighborsClassifier 
 
-def split_data(X, y, split_point=0.3):
-    X_train = X[:len(X) * split_point]
-    X_test = X[len(X) * split_point:]
-    y_train = y[:len(y) * split_point]
-    y_test = y[len(y) * split_point:]
+def randomize_data(X, y):
+    for _ in range(1000):
+        index1 = randrange(len(X)) 
+        index2 = randrange(len(X)) 
+        X[index1], X[index2] = X[index2], X[index1]
+        y[index1], y[index2] = y[index2], y[index1]
+    return X, y
+
+def split_data(X, y, train=0.2):
+    split = int(len(X) * train)
+    X_train = X[split:]
+    X_test = X[:split]
+    y_train = y[split:]
+    y_test = y[:split]
     return X_train, X_test, y_train, y_test
 
 def knn_score(X, y):
@@ -13,7 +23,11 @@ def knn_score(X, y):
     model = KNeighborsClassifier(n_neighbors=3)
     model.fit(X_train, y_train)
     predicted = model.predict(X_test)
-    return (np.sum(predicted = y_test) / len(y_test)) * 100
+    score = 0
+    for i in range(len(predicted)):
+        if predicted[i] == y_test[i]:
+            score += 1
+    return (score / len(y_test)) * 100
 
 def variance_threshold(X, threshold=0):
     new_X = np.copy(X)
@@ -26,10 +40,9 @@ def variance_threshold(X, threshold=0):
         new_X = np.delete(new_X, drop[i] - i, 1)
     return new_X
  
- 
 def forward_search(X, y, features_number=-1):
     if features_number == -1:
-        features_number = np.size(X, axis = 1)
+        features_number = np.size(X, axis=1)
     length = np.size(X, axis = 0)
     best = []
     best_score = 0
@@ -37,14 +50,14 @@ def forward_search(X, y, features_number=-1):
     for i in range(features_number):
         curr = curr_best[:]
         curr_best_score = 0
-        numbers = range(features_number)
+        numbers = list(range(features_number))
         for n in curr:
             numbers.remove(n)
         for num in numbers:
             curr.append(num)
             curr.sort()
             XX = X[:, curr]
-            score = get_score(XX, y)
+            score = knn_score(XX, y)
             if score > curr_best_score:
                 curr_best_score = score
                 curr_best = curr[:]
